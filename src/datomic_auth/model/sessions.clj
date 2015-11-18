8a7068b8c9b8ed3c699b5d740f885f156a1f0f49
Tyler van Hensbergen <tvanhens@gmail.com>
2015-11-17 20:50:06 -0500

    Add valid-token?

--- src/datomic_auth/model/sessions.clj
+++ src/datomic_auth/model/sessions.clj
@@ -2,6 +2,22 @@
   (:require [datomic.api :as d]
             [datomic-auth.db :as db]))
 
+(def users-tokens
+  '[(users-tokens ?user-token ?token ?tokens)
+    [?token :session/token ?user-token]
+    [?token :session/user ?user]
+    [?tokens :session/user ?user]])
+
+(def tx
+  '[(tx ?e ?tx)
+    [?e _ _ ?tx]])
+
+(def by-event
+  '[(by-event ?event ?tokens)
+    [(name ?event) ?event-name]
+    [(keyword "session.event" ?event-name) ?event-ident]
+    [?tokens :session/event ?event-ident]])
+
 (defn- event-ident [kw]
   (->> kw name (keyword "session.event")))
 
@@ -10,3 +26,14 @@
     :session/token token
     :session/user  (db/ident user)
     :session/event (event-ident event)}])
+
+(defn valid-token? [db token]
+  (->> (d/q '{:find  [[?valid ...]]
+              :in    [$ % ?str-token]
+              :where [(users-tokens ?str-token ?this-token ?users-tokens)
+                      (by-event :passwordChanged ?tokens)
+                      (tx ?this-token ?tx1)
+                      (tx ?users-tokens ?tx2)
+                      [(>= ?tx1 ?tx2) ?valid]]}
+            db [users-tokens by-event tx] token)
+       (every? identity)))
