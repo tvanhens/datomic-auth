9cb336b4513b4050859f2336c420714caa4b430f
Tyler van Hensbergen <tvanhens@gmail.com>
2015-11-18 19:59:35 -0500

    Accept conn rather than db

--- src/datomic_auth/lib/auth/core.clj
+++ src/datomic_auth/lib/auth/core.clj
@@ -4,6 +4,7 @@
             [datomic-auth.auth :as auth]
             [datomic-auth.db :as db]
             [datomic-auth.utils :as utils]
+            [datomic.api :as d]
             [ring.util.response :refer :all]))
 
 (defn- token-response [token] (response {:token token}))
@@ -20,29 +21,33 @@
 (defn- generate-token [db username]
   (->> username (users/username->uuid db) auth/generate-token))
 
-(defn register [db username password]
-  (if (users/username-exists? db username)
-    (username-exists-response username)
-    (let [uuid               (utils/uuid)
-          user               (users/create username password uuid)
-          token              (auth/generate-token uuid)
-          {:keys [db-after]} @(db/transact [user (sessions/create token user :registered)])]
-      (token-response token))))
+(defn register [conn username password]
+  (let [db (d/db conn)]
+    (if (users/username-exists? db username)
+      (username-exists-response username)
+      (let [uuid               (utils/uuid)
+            user               (users/create username password uuid)
+            token              (auth/generate-token uuid)
+            {:keys [db-after]} @(db/transact conn [user (sessions/create token user :registered)])]
+        (token-response token)))))
 
-(defn login [db username password]
-  (if (users/login-valid? db username password)
-    (let [token (generate-token db username)]
-      @(db/transact [(sessions/create token (users/map->User {:username username}) :loggedIn)])
-      (token-response token))
-    (invalid-username-password-response)))
-
-(defn change-password [db username old-password new-password confirm]
-  (if (users/login-valid? db username old-password)
-    (if (= new-password confirm)
-      (let [token (generate-token db username)
-            {:keys [db-after]}
-            @(db/transact [(users/change-password username new-password)
-                           (sessions/create token (users/map->User {:username username}) :passwordChanged)])]
+(defn login [conn username password]
+  (let [db (d/db conn)]
+    (if (users/login-valid? db username password)
+      (let [token (generate-token db username)]
+        @(db/transact conn [(sessions/create token (users/map->User {:username username}) :loggedIn)])
         (token-response token))
-      (new-confirm-mismatch-response))
-    (invalid-username-password-response)))
+      (invalid-username-password-response))))
+
+(defn change-password [conn username old-password new-password confirm]
+  (let [db (d/db conn)]
+    (if (users/login-valid? db username old-password)
+      (if (= new-password confirm)
+        (let [token (generate-token db username)
+              {:keys [db-after]}
+              @(db/transact conn
+                            [(users/change-password username new-password)
+                             (sessions/create token (users/map->User {:username username}) :passwordChanged)])]
+          (token-response token))
+        (new-confirm-mismatch-response))
+      (invalid-username-password-response))))
