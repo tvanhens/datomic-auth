4112bd17cbd73f0304b90c7790e3d5d0cc4fe4c3
Tyler van Hensbergen <tvanhens@gmail.com>
2015-11-18 19:16:40 -0500

    Fix login fn

--- src/datomic_auth/lib/auth/core.clj
+++ src/datomic_auth/lib/auth/core.clj
@@ -32,8 +32,7 @@
 (defn login [db username password]
   (if (users/login-valid? db username password)
     (let [token (generate-token db username)]
-      @(db/transact [(sessions/create
-                      token (users/map->User {:username username} :loggedIn))])
+      @(db/transact [(sessions/create token (users/map->User {:username username}) :loggedIn)])
       (token-response token))
     (invalid-username-password-response)))
 
@@ -43,8 +42,7 @@
       (let [token (generate-token db username)
             {:keys [db-after]}
             @(db/transact [(users/change-password username new-password)
-                           (sessions/create
-                            token (users/map->User {:username username}) :passwordChanged)])]
+                           (sessions/create token (users/map->User {:username username}) :passwordChanged)])]
         (token-response token))
       (new-confirm-mismatch-response))
     (invalid-username-password-response)))
