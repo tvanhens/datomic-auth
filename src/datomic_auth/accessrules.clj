e2ef06ed432fe8802d108013a92ca6046ea1d4ed
Tyler van Hensbergen <tvanhens@gmail.com>
2015-11-21 09:41:05 -0500

    Use buddy accessrules

---
+++ src/datomic_auth/accessrules.clj
@@ -0,0 +1,3 @@
+(ns datomic-auth.accessrules)
+
+(def rules [])
--- src/datomic_auth/auth.clj
+++ src/datomic_auth/auth.clj
@@ -4,7 +4,9 @@
             [buddy.sign.jwe :as jwe]
             [buddy.auth.backends.token :as token]
             [buddy.auth.middleware :as middleware]
-            [datomic-auth.utils :as utils]))
+            [buddy.auth.accessrules :as accessrules]
+            [datomic-auth.utils :as utils]
+            [datomic-auth.accessrules :refer [rules]]))
 
 (def secret (hash/sha256 "notasecret"))
 
@@ -22,12 +24,9 @@
   (fn [{:keys [identity] :as request}]
     (handler (update request :identity parse-identity))))
 
-(defn wrap-authentication-rules [handler]
-  (fn [{:keys [identity] :as request}]
-    (handler request)))
-
 (defn wrap-authentication [handler]
   (-> handler
-      wrap-authentication-rules
       wrap-parse-identity
+      (accessrules/wrap-access-rules {:rules   rules
+                                      :default :reject})
       (middleware/wrap-authentication backend)))
